a135f5fd3efd80947f443fa939d2abc866ab1ab5
Hecton <hectonpdomingos@gmail.com>
2016-11-22 12:45:45 -0200

    C

---
+++ aprendizadojava/Car.java
@@ -0,0 +1,18 @@
+package aprendizadojava;
+
+/**
+ * Created by Hecton on 22/11/2016.
+ */
+public class Car {
+
+    private int portas;
+    private int rodas;
+    private String modelo;
+    private String motor;
+    private String cor;
+
+    //method
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+}
---
+++ aprendizadojava/Car_rent.java
@@ -0,0 +1,16 @@
+package aprendizadojava;
+
+/**
+ * Created by Hecton on 22/11/2016.
+ */
+public class Car_rent {
+
+    public static void main(String[] args){
+    //object
+        Car ferrari = new Car();
+        Car porshe = new Car();
+
+        ferrari.setModelo("F350");
+    }
+}
+
